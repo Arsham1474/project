3abd11347f1f65507c0e7aacdc9e2f1dcc910b98
arsham1474 <arshamardizi@gmail.com>
2025-11-06 06:47:49 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+<head>
+    <!----------------BOOTSTRAP------------------->
+    <!----------------Latest Compile and minified CSS---->
+    <link rel="stylesheet" href="css/fa-bootstrap.css">
+    <!---------------jQuery library------------->
+    <script src="js/jquery.min.js"></script>
+    <!-------Latest compoil Javascript------>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/MyStyleCssArsham.css">
+    <!-----------BOOTSTRAP------------------------->
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collaps" data-target="menue">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                    <a href="#" class="navbar-brand">
+                        <span class="glyphicon glyphicon-apple">شهدای 9 دی</span>
+                    </a>
+            </div>
+            <div class="collapse navbar-collapse" id="menue">
+                <ul class="nav navbar-nav">
+                    <li><a href="3">خانه</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">  سالن ها
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">شهید دامرودی</a> </li>
+                            <li><a href="#">شهید ابراهیم هادی</a> </li>
+                            <li><a href="#">شهید شجیعی</a> </li>
+                            <li><a href="#">شهید کاوه</a> </li>
+                            <li><a href="#">شهید فهمیده</a> </li>
+                            <li><a href="#">پینت بال </a> </li>
+                        </ul>
+                    </li>
+                    <li><a href="3">تماس با ما</a></li>
+                </ul>
+                <form action="#" class="navbar-form">
+                    <div class="input-group navbar-right">
+                        <input type="text" class="form-control" placeholder="جستجو ....">
+                        <div class="input-group-btn">
+                            <button type="submit" class="btn btn-info">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <ul class="nav navbar-nav navbar-left">
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-log-in">ورود کاربران</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-log-in">ورود مدیران</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-log-in">ثبت نام </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
